a229239de9d7ccae8f4b87e8e06848ddac142e5c
Abhinanda24 <nandaabhi580@gmail.com>
2026-01-28 22:01:01 +0530

    Updated

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -7,8 +7,13 @@ class Main{
         // obj.rightangle_triangle_number(5);
         // obj.reverse_triangle(5);
         // obj.reverse_number_triangle(5);
-        obj.pyramid(5);
-    }
+        // obj.pyramid(5);
+        // obj.sharp_star(5);
+        // obj.zero_one(5);
+        // obj.butterfly(5);
+        // obj.number_Rtriangle(5);
+        // obj.char_Triangle(5);
+        obj.square_outer(5);    }
 }
 class pattern{
     public void square(int n){
@@ -78,6 +83,108 @@ class pattern{
             }
             for(int k=1;k<=2*(n-i)+1;k++){
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public void sharp_star(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+    public void zero_one(int n){
+
+        for(int i=1;i<=n;i++){
+            int start;
+            if(i%2==0){
+                    start=0;
+                }
+                else{
+                    start=1;
+                }
+            for(int j=1;j<=i;j++){
+                
+                System.out.print(start);
+                start=1-start;
+            }
+            System.out.println();
+        }
+    }
+    public void butterfly(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+j);
+            }
+            for(int k=1;k<=2*(n-i);k++){
+                System.out.print("  ");
+            }
+            for(int l=i;l>=1;l--){
+                System.out.print(" "+l);
+            }
+            System.out.println();
+    
+    }
+}
+    public void number_Rtriangle(int n){
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+num);
+                num++;
+            }
+            System.out.println();
+
+        }
+    }
+    public void char_Triangle(int n){
+        for(int i=1;i<=n;i++){
+            char ch='A';
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+ch);
+                ch++;
+            }
+            System.out.println();
+        }
+        System.out.println("2");
+         for(int i=1;i<=n;i++){
+            char ch='A';
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+         }
+         System.out.println(3);
+         char ch='A';
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch);
+
+            }
+            ch++;
+            System.out.println();
+         }
+    }
+    public void square_outer(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if((i==1 || i==n)||(j==1||j==n)){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
